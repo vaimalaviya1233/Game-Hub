24e1ca552a24b1c5e603d71d77d0b1d71f06195d
FeDeveloper <federico.gobbi2009@gmail.com>
2026-04-11 18:41:11 +0200

    fixes

--- app/src/main/kotlin/com/fedeveloper95/games/MainActivity.kt
+++ app/src/main/kotlin/com/fedeveloper95/games/MainActivity.kt
@@ -24,7 +24,6 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.ExperimentalFoundationApi
